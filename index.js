20bdc3bfefbc5b2c3fa9f2f47950bd42cedf485e
Damaris Virtan <virtan.alexandra@gmail.com>
2021-08-13 09:21:09 +0300

    feat: map component details

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var cors = require('cors')
+const fetch = require('node-fetch');
+const baseUrl = 'http://localhost';
+const authServiceUrl = baseUrl + ':8082';
+const projServiceUrl = baseUrl + ':8083';
 const app = express()
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,23 +15,66 @@ app.use(bodyParser.raw());
 const Component = require('./models/component')
 const port = 8087
 
-var mongoDB = 'mongodb+srv://Damaris:12345@cluster0.gdp4f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://cata:cata@cluster0.wcbqw.mongodb.net/first?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.post('/component', async (req, res) => {
     let newComponent = req.body
-    var addComponent=new Component({ name:newComponent.name})
+    console.log(newComponent)
+    var addComponent=new Component({ name:newComponent.name, description:newComponent.description, user_id:newComponent.user_id, project_id:newComponent.project_id})
     await Component.create(addComponent)
     res.send(newComponent)
 })
 
 app.get('/component', async (req, res) =>{
-    // const record= await Project.find({'type':req.query.type}).exec()
-    const record= await Component.find({})
-    console.log(record)
-    res.json(record)
+    let result = [];
+    let record = await Component.find({})
+    const userIds = record.map(c => c.user_id);
+    const projectIds = record.map(c => c.project_id);
+
+    let users = [];
+    let projects = [];
+    await fetch(authServiceUrl + '/allusers', 
+    { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userIds)
+    })
+    .then(res => res.json())
+    .then(data => users = data);
+
+    //projects
+    await fetch(projServiceUrl + '/allprojects', 
+    { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(projectIds)
+    })
+    .then(res => res.json())
+    .then(data => projects = data);
+
+    for (let index = 0; index < record.length; index++) {
+        if (!users[index]) {
+            users[index] = { username: "no user" };
+        }
+        const componentDTO = {
+            _id: record[index]._id,
+            name: record[index].name,
+            description: record[index].description,
+            username: users[index].username,
+            projectName: projects[index].name,
+            issuesNo: 0
+            // _id: record[index]._id,
+        }
+        result.push(componentDTO);   
+    } 
+    res.json(result)
 })
 
 app.listen(port, () => {
